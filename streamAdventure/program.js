5dcb1bff0500dd9558538d61405566c8c9968789
Josh Smith <jdsmith@magellanhealth.com>
2017-03-02 16:43:41 -0700

    Continue through stream-adventure

--- streamAdventure/program.js
+++ streamAdventure/program.js
@@ -1,15 +1,15 @@
-//1
+//BEEP BOOP
 //console.log('beep boop')
 
-//2
+//MEET PIPE
 // var fs = require('fs')
 // var file = process.argv[2]
 // fs.createReadStream(file).pipe(process.stdout)
 
-//3
-//process.stdin.pipe(process.stdout)
+//INPUT OUTPUT
+// process.stdin.pipe(process.stdout)
 
-//4
+//TRANSFORM
 // var through = require('through2')
 // var stream = through(write)
 
@@ -17,14 +17,14 @@
 //     this.push(buffer.toString().toUpperCase())
 //     next()
 // }
-//THIS DOES WHAT THE ABOVE DOES AS WELL, BUT USES through2-map instead (like learnyounode #12)
+////THIS DOES WHAT THE ABOVE DOES AS WELL, BUT USES through2-map instead (like learnyounode #12)
 // var through = require('through2-map')
 // var stream = through(function (chunk) {
 //     return chunk.toString().toUpperCase()
 // })
 // process.stdin.pipe(stream).pipe(process.stdout)
 
-//5
+//LINES
 // var split = require('split')
 // var through = require('through2')
 // var map = require('through2-map')
@@ -44,7 +44,7 @@
 //     }))
 //     .pipe(process.stdout)
 
-//6
+//CONCAT
 // var concat = require('concat-stream')
 
 // process.stdin.pipe(concat(function(buffer) {
@@ -52,3 +52,98 @@
 //     // process.stdout.write('\n')
 //     console.log(buffer.toString().split('').reverse().join(''))
 // }))
+
+//HTTP SERVER
+// var http = require('http')
+// var fs = require('fs')
+// var t2map = require('through2-map')
+
+// var port = process.argv[2]
+
+// var server = http.createServer(function (req, res) {
+//     if (req.method === 'POST') {
+//         //USE through2-map
+//         req.pipe(t2map(function (chunk) {
+//             return chunk.toString().toUpperCase()
+//         })).pipe(res)
+//         //USE through2
+//         // req.pipe(through(function (buf, _, next) {
+//         //     this.push(buf.toString().toUpperCase());
+//         //     next();
+//         // })).pipe(res);
+//     } else {
+//         res.end('send a POST!\n')
+//     }
+// })
+// server.listen(port)
+
+//HTTP CLIENT
+// var request = require('request')
+// var r = request.post('http://localhost:8099')
+// process.stdin.pipe(r).pipe(process.stdout)
+
+//WEBSOCKETS
+// var ws = require('websocket-stream')
+// var stream = ws('ws://localhost:8099')
+// stream.write('hello\n')
+
+//HTML STREAM
+// var t2map = require('through2-map')
+// var trumpet = require('trumpet')
+
+// var tr = trumpet()
+
+// var getLoud = function (chunk) {
+//     return chunk.toString().toUpperCase();
+// }
+
+// var loud = tr.select('.loud').createStream()
+// loud.pipe(t2map(getLoud)).pipe(loud)
+// process.stdin.pipe(tr).pipe(process.stdout)
+
+//DUPLEXER
+// var spawn = require('child_process').spawn
+// var duplexer2 = require('duplexer2')
+
+// module.exports = function (cmd, args) {
+//     var child = spawn(cmd, args)
+//     return duplexer2(child.stdin, child.stdout)
+// }
+
+//DUPLEXER REDUX
+// var duplexer = require('duplexer2').obj
+// var through = require('through2').obj
+
+// module.exports = function (counter) {
+//     var counts = {}
+//     return duplexer({writableObjectMode:true}, through(record_count, set_count), counter)
+
+//     function record_count(obj) {
+//         var country = obj.country
+//         var count = counts[country] || 0
+//         counts[country] += 1
+//     }
+
+//     function set_count() {
+//         counter.setCounts(counts)
+//         counts = {}
+//     }
+// }
+
+var duplexer = require('duplexer2');
+var through = require('through2').obj;
+
+module.exports = function (counter) {
+    var counts = {};
+    var input = through(write, end);
+    return duplexer({objectMode: true}, input, counter);
+
+    function write (row, _, next) {
+        counts[row.country] = (counts[row.country] || 0) + 1;
+        next();
+    }
+    function end (done) {
+        counter.setCounts(counts);
+        done();
+    }
+};
